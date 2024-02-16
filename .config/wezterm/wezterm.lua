88e3759957b694c14759ef1a1b47f3d21dfb29e8
Mats Faugli <mats.faugli@gmail.com>
2024-02-16 01:07:30 +0100

    wezterm: initial config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require("wezterm")
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.color_scheme = "Catppuccin Mocha"
+
+-- config.window_background_opacity = 0.90
+-- config.font = wezterm.font("Monaspace Neon", { weight = "Light" })
+
+-- Use a different font for italic text
+-- Makes comments in code look hand written
+-- config.font_rules = { { italic = true, font = wezterm.font("Monaspace Radon") } }
+-- config.font_size = 14.0
+
+-- Spawn a fish shell in login mode
+config.default_prog = { "/home/linuxbrew/.linuxbrew/bin/fish", "-l" }
+
+return config
