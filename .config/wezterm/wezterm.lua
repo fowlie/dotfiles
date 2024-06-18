d8be6abd995687797a0afcc0de442596c3c488da
Mats Faugli <mats.faugli@gmail.com>
2024-06-18 13:26:54 +0200

    chore: cleanup

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,14 +10,6 @@ config.window_decorations = "NONE"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 1.0
 
--- config.window_background_opacity = 0.90
--- config.font = wezterm.font("Monaspace Neon", { weight = "Light" })
-
--- Use a different font for italic text
--- Makes comments in code look hand written
--- config.font_rules = { { italic = true, font = wezterm.font("Monaspace Radon") } }
--- config.font_size = 14.0
-
 -- Spawn a fish shell in login mode
 config.default_prog = { "/home/linuxbrew/.linuxbrew/bin/fish", "-l" }
 
