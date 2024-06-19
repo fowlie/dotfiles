2bfe33f8376b16a689070a5dc331714f2f67027e
Mats Faugli <mats.faugli@gmail.com>
2024-06-19 10:50:26 +0200

    tmux: make basedir as window labels

--- .tmux/plugins/tmux/custom/pomodoro.sh
+++ .tmux/plugins/tmux/custom/pomodoro.sh
@@ -1,9 +1,9 @@
 # Requires https://github.com/olimorris/tmux-pomodoro-plus
 
 show_pomodoro() {
-  local color module
-  color="$( get_tmux_option "@catppuccin_pomodoroplus_color" "$thm_green" )"
-  module=$( build_status_module "$1" "" "$color" "#{pomodoro_status}" )
-  echo "$module"
+	local color module status
+	color="$(get_tmux_option "@catppuccin_pomodoroplus_color" "$thm_orange")"
+	status="$(get_tmux_option "@catppuccin_pomodoroplus_status" "#{pomodoro_status}")"
+	module=$(build_status_module "$1" "" "$color" "$status")
+	echo "$module"
 }
-
