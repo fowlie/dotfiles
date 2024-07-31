055f246e9f6215d00f5f965cc31f576f9c714190
Mats Faugli <mats.faugli@gmail.com>
2024-07-31 10:02:43 +0200

    lazyvim: window navigation colemak style

--- .config/lazyvim/lua/config/keymaps.lua
+++ .config/lazyvim/lua/config/keymaps.lua
@@ -5,3 +5,13 @@ local zen_mode = require("zen-mode")
 
 vim.keymap.set("n", "<leader>uz", zen_mode.toggle, { desc = "Toggle zen mode" })
 vim.keymap.set("n", "<leader>ub", "<cmd>GitBlameToggle<CR>", { desc = "Toggle git blame" })
+
+-- Navigate between windows Colemak style
+vim.keymap.del("n", "<C-h>")
+vim.keymap.del("n", "<C-j>")
+vim.keymap.del("n", "<C-k>")
+vim.keymap.del("n", "<C-l>")
+vim.keymap.set("n", "<C-left>", "<C-w>h", { desc = "Go to Left Window" })
+vim.keymap.set("n", "<C-down>", "<C-w>j", { desc = "Go to Lower Window" })
+vim.keymap.set("n", "<C-up>", "<C-w>k", { desc = "Go to Upper Window" })
+vim.keymap.set("n", "<C-right>", "<C-w>l", { desc = "Go to Right Window" })
